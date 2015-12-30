d64d29856d5b1c681de3b5deb54a99eebd59a6ef
leleliu008 <792793182@qq.com>
2015-12-30 18:51:18 +0800

    添加登陆、关于、重置密码页面

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,16 @@ router.get('/login', function(req, res, next) {
   res.render('login', {});
 });
 
+/* 关于. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {});
+});
+
+/* 关于. */
+router.get('/change-password', function(req, res, next) {
+  res.render('change-password', {});
+});
+
 /*  后台. */
 router.get('/admin', function(req, res, next) {
   res.render('admin', {});
