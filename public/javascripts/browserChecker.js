2cd0b1116a5f347be8e900cf8bce1376d5d62afe
leleliu008 <792793182@qq.com>
2016-01-25 01:36:56 +0800

    完善

--- public/javascripts/browserChecker.js
+++ public/javascripts/browserChecker.js
@@ -6,7 +6,8 @@ var browser = navigator.appName;
 var version = parseFloat(navigator.appVersion);
 
 if (browser == "Microsoft Internet Explorer") {
-    if(version <= 4) {
+    if(version <= 3) {
+        //如果是IE8一下的IE内核，提示用户
         alert("鄙视使用IE的人，如果想看，请使用Google-Chrome、Firefox等浏览器");
     }
 }
