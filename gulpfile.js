76c69042aad67349c651f5ea8903136b629dd9ed
leleliu008 <792793182@qq.com>
2019-02-20 17:59:36 +0800

    优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,74 @@
-var outputDir       = 'build';
-var outputPublicDir = outputDir + '/public';
-var outputJSDir     = outputPublicDir + '/js';
-var outputCSSDir    = outputPublicDir + '/css';
-var outputViewsDir  = outputDir + '/views';
-
-var pump      = require('pump');
-var gulp      = require('gulp');
-var uglify    = require('gulp-uglify');
-var cleanCSS  = require('gulp-clean-css');
-var minifyejs = require('gulp-minify-ejs');
+const outputDir       = 'build';
 
 //https://github.com/mafintosh/pump
+const pump      = require('pump');
+
 //https://github.com/gulpjs/gulp
+const gulp      = require('gulp');
+
 //https://github.com/terinjokes/gulp-uglify
+const uglify    = require('gulp-uglify');
+
 //https://github.com/scniro/gulp-clean-css
+const cleanCSS  = require('gulp-clean-css');
+
 //https://github.com/cynosureabu/gulp-min-ejs
+const minifyejs = require('gulp-minify-ejs');
 
+const tasks = {
+    //压缩src/public/js目录下的js文件
+    compressJS: callback => {
+        const option = {
+            mangle : {
+                toplevel: true,
+                keep_fnames: true,
+                reserved: ['$']
+            }
+        };
+        pump([
+            gulp.src('src/public/js/**/*.js'),
+            uglify(option),
+            gulp.dest(outputDir + '/public/js/')
+        ], callback);
+    },
 
-//1、压缩、混淆src/public/js目录下的js文件
-gulp.task('compress-js', function (cb) {
-  var option = {
-    mangle : {
-        toplevel: true,
-        keep_fnames: true,
-        reserved: ['$']
+    //处理src/public/css目录下的css文件中的空行
+    cleanCSS: callback => {
+        pump([
+            gulp.src('src/public/css/*.css'),
+            cleanCSS({compatibility: 'ie8'}),
+            gulp.dest(outputDir + '/public/css/')
+        ], callback);
+    },
+    
+    //复制src/public目录下的其他文件
+    copyPublic: callback => {
+        pump([
+            gulp.src(['src/public/**', '!src/public/js/**/*.js', '!src/public/css/**/*.css']),
+            gulp.dest(outputDir + '/public/')
+        ], callback);
+    },
+
+    //处理src/private/views目录下的ejs模板中的空白行
+    minifyejs: callback => {
+        pump([
+            gulp.src('src/private/views/**/*.ejs'),
+            minifyejs(),
+            gulp.dest(outputDir + '/private/views/')
+        ], callback);
+    },
+
+    //复制src/private目录下的其他文件
+    copyPrivate: callback => {
+        pump([
+            gulp.src(['src/private/**', '!src/private/views/**/*.ejs']),
+            gulp.dest(outputDir + '/private/')
+        ], callback);
     }
-  };
-  pump([
-        gulp.src('src/public/js/**/*.js'),
-        uglify(option),
-        gulp.dest(outputJSDir)
-    ],
-    cb
-  );
-});
+};
 
-//2、处理src/public/css目录下的css文件中的空行
-gulp.task('compress-css', function(callback) {
-    gulp.src('src/public/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(outputCSSDir));
-});
+for(var key in tasks) {
+    gulp.task(key, tasks[key]);
+}
 
-//3、处理src/views目录下的ejs模板中的空白行
-gulp.task('compress-views', function(callback) {
-    gulp.src('src/views/**/*.ejs')
-        .pipe(minifyejs())
-        .pipe(gulp.dest(outputViewsDir));
-});
-
-//4、复制不需要做处理的文件
-gulp.task('copy-files', function(callback) {
-    gulp.src('src/public/images/**').pipe(gulp.dest(outputDir + '/public/images'));
-    gulp.src('src/public/font/**').pipe(gulp.dest(outputDir + '/public/font'));
-    gulp.src(['src/public/bower_components/**']).pipe(gulp.dest(outputDir + '/public/bower_components'));
-    gulp.src('src/*.js').pipe(gulp.dest(outputDir));
-    gulp.src('src/utils/**/*.js').pipe(gulp.dest(outputDir + '/utils/'));
-    gulp.src('src/routes/**/*.js').pipe(gulp.dest(outputDir + '/routes/'));
-    gulp.src(['src/views/**', '!src/views/**/*.ejs']).pipe(gulp.dest(outputViewsDir));
-});
-
-gulp.task('default', ['copy-files', 'compress-js', 'compress-css', 'compress-views']);
+gulp.task('default', Object.keys(tasks));
--- src/private/app.js
+++ src/private/app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', require('./routes/index'));
 app.use('/about', require('./routes/about'));
